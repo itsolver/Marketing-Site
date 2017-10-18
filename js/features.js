bb322e9a243f820b282a050119b5cbce0a3c9561
Mike Neumegen <mneumegen@gmail.com>
2017-10-18 12:05:52 -0700

    new demo videos

--- js/features.js
+++ js/features.js
@@ -1,6 +1,6 @@
 var videoClick = function(el) {
 	el.preventDefault();
-	document.body.insertAdjacentHTML('beforeend', '<div class="overlay" id="overlay"><div class="video-container"><div class="video-embed"><iframe src="https://www.youtube.com/embed/' + this.dataset.video + '?loop=1&amp;rel=0&amp;autoplay=1&amp;showinfo=0&amp;playlist=' + this.dataset.video + '" frameborder="0" allowfullscreen=""></iframe></div></div></div>');
+	document.body.insertAdjacentHTML('beforeend', '<div class="overlay" id="overlay"><div class="video-container"><div class="video-embed"><iframe src="https://www.youtube.com/embed/' + this.dataset.video + '?rel=0&amp;autoplay=1&amp;showinfo=0" frameborder="0" allowfullscreen="" width="1280" height="720"></iframe></div></div></div>');
 
 	document.body.classList.add("no-scroll");
 	console.log('no-scroll');
@@ -17,3 +17,9 @@ for (i = 0; i < videoLinks.length; i++) {
 	videoLinks[i].addEventListener('click', videoClick);
 }
 
+window.onkeyup = function (event) {
+	var $overlay = document.getElementById('overlay');
+	if (event.keyCode == 27 && $overlay) {
+		$overlay.remove();
+	}
+};
