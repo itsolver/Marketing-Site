b7a30556c2b2f339655ed6f64048f031a2bffa78
Mike Neumegen <mneumegen@gmail.com>
2017-10-17 18:54:02 -0700

    fix up demo videos

--- js/features.js
+++ js/features.js
@@ -1,10 +1,13 @@
 var videoClick = function(el) {
 	el.preventDefault();
-	document.body.insertAdjacentHTML('beforeend', '<div class="overlay" id="overlay"><div class="video-container"><div class="video-embed"><iframe src="https://www.youtube.com/embed/' + this.dataset.video + '?loop=1&amp;rel=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0&amp;playlist=' + this.dataset.video + '" frameborder="0" allowfullscreen=""></iframe></div></div></div>');
+	document.body.insertAdjacentHTML('beforeend', '<div class="overlay" id="overlay"><div class="video-container"><div class="video-embed"><iframe src="https://www.youtube.com/embed/' + this.dataset.video + '?loop=1&amp;rel=0&amp;autoplay=1&amp;showinfo=0&amp;playlist=' + this.dataset.video + '" frameborder="0" allowfullscreen=""></iframe></div></div></div>');
 
+	document.body.classList.add("no-scroll");
+	console.log('no-scroll');
 	document.getElementById('overlay').addEventListener('click', function(el) {
 		el.preventDefault();
 		this.parentNode.removeChild(this);
+		document.body.classList.remove("no-scroll");
 	});
 };
 
